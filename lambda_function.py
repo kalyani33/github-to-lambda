af401a3225b3837b3dba044bd4ec6e6b23573e77
Kalyani <kalyanib67@gmail.com>
2024-03-09 23:55:59 +0530

    added buildspec.yml file for CICD

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 import requests
 import pandas as pd
 
-def handler_handler(event,context):
+def lambda_handler(event,context):
     print("Event data -> ",event)
     response = requests.get("https://www.google.com/")
     print(response.text)
